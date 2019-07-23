d09abf94030ed338a80eedecefaa11a96bd9a798
ahmetbcakici <ahmetbcakici@gmail.com>
2019-07-23 20:44:12 +0300

    Update Commit

--- app.py
+++ app.py
@@ -1,28 +1,26 @@
-from flask import Flask,render_template,jsonify,request
+from flask import Flask,render_template,request,jsonify
 from flask_mysqldb import MySQL
 app = Flask(__name__)
 mysql = MySQL(app)
 
-#Enter your database informations 
+#Enter here your database informations 
 app.config["MYSQL_HOST"] = ""
 app.config["MYSQL_USER"] = ""
 app.config["MYSQL_PASSWORD"] = ""
 app.config["MYSQL_DB"] = ""
-app.config["MYSQL_CURSORCLASS"] = "DictCursor"
+app.config["MYSQL_CURSORCLASS"] = ""
 
 @app.route("/")
 def index():
     return render_template("index.html")
 @app.route("/livesearch",methods=["POST","GET"])
 def livesearch():
-    searchbox = request.form.get('text')
+    searchbox = request.form.get("text")
     cursor = mysql.connection.cursor()
-    query = "select word_eng from words where word_eng LIKE '{}%' order by word_eng".format(searchbox)#This is just exampel query , you should replace it with your query
+    query = "select word_eng from words where word_eng LIKE '{}%' order by word_eng".format(searchbox)#This is just example query , you should replace field names with yours
     cursor.execute(query)
     result = cursor.fetchall()
     return jsonify(result)
-
-
 
 if __name__ == "__main__":
     app.run(debug=True)
